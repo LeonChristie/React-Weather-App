d274fcc073d052d43f7e38bd003d613b8a041d27
LeonChristie <110294002+LeonChristie@users.noreply.github.com>
2023-01-20 11:55:10 +0000

    Revert "README and project descriptive docs"
    This reverts commit b2d03ede4851bf4601251e870b5500e67f9583a5.

--- portfolio-react-weather-app/src/App.js
+++ portfolio-react-weather-app/src/App.js
@@ -9,6 +9,8 @@ function App() {
   const [location, setLocation] = useState();
   const [currentData, setCurrentData] = useState();
   const [forecastData, setForecastData] = useState();
+  const [forecastIcons, setForecastIcons] = useState();
+
 
   useEffect(() => {
     const fetchCoords = async () => {
@@ -22,7 +24,7 @@ function App() {
       const currentWeather = await currentRes.json();
       currentWeather.iconurl = `http://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@2x.png`;
       setCurrentData(currentWeather);
-
+      console.log(currentWeather);
       const resFore = await fetch(
         `https://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&units=metric&appid=${process.env.REACT_APP_WEATHER_API}`
       );
@@ -40,7 +42,7 @@ function App() {
 
   return (
     <div className="container">
-      <Search submitSearch={submitSearch} />
+      <Search submitSearch={submitSearch}/>
       {typeof currentData != "undefined" ? (
         <CurrerntWeather currentData={currentData} />
       ) : (
